91187463900ffd1c1cf649f6935ca61a9dc4018f
Michael Giesler <michael.giesler1@icloud.com>
2024-11-15 14:37:51 -0700

    update the clock display

--- app/controllers/ClockController.js
+++ app/controllers/ClockController.js
@@ -10,3 +10,11 @@ export class ClockController {
         setInterval(() => this.drawClock(), 1000);
     }
 
+    drawClock() {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const timeString = `${hours}:${minutes}`;
+        setHTML('clock', `<h1>${timeString}</h1>`);
+    }
+}
