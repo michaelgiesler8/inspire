5c5de4d3a056b7b6310b6db889f6ec06b1066a7f
Michael Giesler <michael.giesler1@icloud.com>
2024-11-15 12:38:21 -0700

    fetches the current weather

--- app/services/WeatherService.js
+++ app/services/WeatherService.js
@@ -2,3 +2,15 @@ import { api } from './AxiosService.js';
 import { AppState } from '../AppState.js';
 import { Weather } from '../models/Weather.js';
 
+class WeatherService {
+    async getWeather() {
+        try {
+            const res = await api.get('/api/weather');
+            AppState.weather = new Weather(res.data);
+        } catch (error) {
+            console.error('Error fetching weather:', error);
+        }
+    }
+}
+
+export const weatherService = new WeatherService();
