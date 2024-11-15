b6217d55480f21d4ba1ff1527cca2598fc87d203
Michael Giesler <michael.giesler1@icloud.com>
2024-11-15 12:35:57 -0700

    fetches a background image

--- app/services/ImageService.js
+++ app/services/ImageService.js
@@ -1,3 +1,16 @@
 import { api } from './AxiosService.js';
 import { AppState } from '../AppState.js';
 import { Image } from '../models/Image.js';
+
+class ImageService {
+    async getImage() {
+        try {
+            const res = await api.get('/api/images');
+            AppState.image = new Image(res.data);
+        } catch (error) {
+            console.error('Error fetching image:', error);
+        }
+    }
+}
+
+export const imageService = new ImageService();
