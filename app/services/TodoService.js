53919cdb4df7d63666faa673bd62f28eb511405f
Michael Giesler <michael.giesler1@icloud.com>
2024-11-15 12:43:27 -0700

    fetches all todos

--- app/services/TodoService.js
+++ app/services/TodoService.js
@@ -1,3 +1,14 @@
 import { api } from './AxiosService.js';
 import { AppState } from '../AppState.js';
 import { Todo } from '../models/Todo.js';
+
+class TodoService {
+    async getTodos() {
+        try {
+            const res = await api.get('/api/todos');
+            AppState.todos = res.data.map((t) => new Todo(t));
+        } catch (error) {
+            console.error('Error fetching todos:', error);
+        }
+    }
+
