1078f4f60c33544f21937288e10253e007e82212
Michael Giesler <michael.giesler1@icloud.com>
2024-11-15 12:28:43 -0700

    fetches the user's account info

--- app/services/AccountService.js
+++ app/services/AccountService.js
@@ -1,3 +1,16 @@
 import { api } from './AxiosService.js';
 import { AppState } from '../../AppState.js';
 import { Account } from '../models/Account.js';
+
+class AccountService {
+  async getAccount() {
+    try {
+      const res = await api.get('/account');
+      AppState.account = new Account(res.data);
+    } catch (err) {
+      console.error('Error getting account:', err);
+    }
+  }
+}
+
+export const accountService = new AccountService();
