387172f437d2a379cb2f0b5cec253737186c8620
Michael Giesler <michael.giesler1@icloud.com>
2024-11-15 12:27:57 -0700

    handles fetching user account data

--- app/services/AccountService.js
+++ app/services/AccountService.js
@@ -1,23 +1,3 @@
-import { AppState } from '../AppState.js'
-import { Account } from '../models/Account.js'
-import { logger } from '../utils/Logger.js'
-import { api } from './AxiosService.js'
-
-class AccountService {
-  async getAccount() {
-    try {
-      const res = await api.get('/account')
-      AppState.account = new Account(res.data)
-    } catch (err) {
-      logger.error(err)
-    }
-  }
-
-  async editAccount(accountData) {
-    const res = await api.put('/account', accountData)
-    AppState.account = new Account(res.data)
-  }
-
-}
-
-export const accountService = new AccountService()
+import { api } from './AxiosService.js';
+import { AppState } from '../../AppState.js';
+import { Account } from '../models/Account.js';
